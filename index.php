96885db444c0283af28cc084e47f3236e8db1a49
natsu <nixi3610@gmail.com>
2019-10-23 00:14:03 +0900

    ブログ風。

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>ねこブログ</title>
+		<link rel ="stylesheet" href="style.css">
+		<meta name="viewport" content="width=device-width">
+		<meta name="discription" content="かわいい猫のことについて書いたブログです。">
+		<link rel="icon" href="favicon.ico" >
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
+		<script src="js/jquery-3.3.1.min.js"></script>
+	</head>
+	<body>
+			<div id="container">
+			<header>
+				<div class="title">
+					<h1 class="wf-nicomoji">ねこブログ<i class="fas fa-paw"></i></h1>
+					<div id="nToggle">
+						<div>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="#"><i class="fas fa-fish"></i>リンク１</a></li>
+						<li><a href="#"><i class="fas fa-fish"></i>リンク２</a></li>
+						<li><a href="#"><i class="fas fa-fish"></i>リンク３</a></li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+			<div class="pop">
+					<article>
+						<div class="image i1">
+							<a href="#"></a>
+						</div>
+						<h3><a href="#">タイトル</a></h3>
+						<div class="read">
+							<a href="#">READ MORE</a>
+						</div>
+					</article>
+					<article>
+						<div class="image i2">
+							<a href="#"></a>
+						</div>
+						<h3><a href="#">タイトル</a></h3>
+						<div class="read">
+							<a href="#">READ MORE</a>
+						</div>
+					</article>
+					<article>
+						<div class="image i3">
+							<a href="#"></a>
+						</div>
+							<h3><a href="#">タイトル</a></h3>
+						<div class="read">
+							<a href="#">READ MORE</a>
+						</div>
+					</article>
+				</div>
+				<div class="flex">
+					<div class="content">
+						<article>
+							<time>2019-3-10</time>
+							<h3><a href="#">タイトル</a></h3>
+							<p class="cat">カテゴリ</p>
+							<div class="image i4">
+								<a class="mainA" href="#"></a>
+								<p>記事の内容。</p>
+							</div>
+							<div class="read">
+								<a href="#">READ MORE</a>
+							</div>
+						</article>
+						<article>
+							<time>2019-3-10</time>
+							<h3><a href="#">タイトル</a></h3>
+							<p class="cat">カテゴリ</p>
+							<div class="image i1">
+								<a class="mainA" href="#"></a>
+								<p>記事の内容。</p>
+							</div>
+							<div class="read">
+								<a href="#">READ MORE</a>
+							</div>
+						</article>
+						<article>
+							<time>2019-3-10</time>
+							<h3><a href="#">タイトル</a></h3>
+							<p class="cat">カテゴリ</p>
+							<div class="image i2">
+								<a class="mainA" href="#"></a>
+								<p>記事の内容。</p>
+							</div>
+							<div class="read">
+								<a href="#">READ MORE</a>
+							</div>
+						</article>
+						<article>
+							<time>2019-3-10</time>
+							<h3><a href="#">タイトル</a></h3>
+							<p class="cat">カテゴリ</p>
+							<div class="image i3">
+								<a class="mainA" href="#"></a>
+								<p>記事の内容。</p>
+							</div>
+							<div class="read">
+								<a href="#">READ MORE</a>
+							</div>
+						</article>
+						<div class="next">
+							<a class="black" href="#">次のページ<i class="fas fa-angle-double-right"></i></a>
+						</div>
+					</div>
+					<aside>
+						<article class="wrap">
+							<p><img class="auth" src="image/auth.jpg" alt="著者"></p>
+							<h3 class="auth">著者</h3>
+							<p class="auth">著者紹介。実績など。</p>
+							<ul class="auth">
+								<li><a href="#">コンサル依頼の詳細</a></li>
+								<li><a href="#">Web制作依頼の詳細</a></li>
+								<li><a href="#">ライティング依頼来の詳細</a></li>
+								<li><a href="#">お問合せ</a></li>
+							</ul>
+						</article>
+						<div class="search wrap">
+							<form method="#" action="post" class="search">
+								<p><input type="text" name="search" id="search" placeholder="search for…"></p>
+								<p><input type="submit" value="検索"></p>
+							</form>
+						</div>
+						<div class="rank wrap">
+								<h3 class="rank">よく読まれる記事</h3>
+							<div class="cont">
+								<p><a href="#"><img src="image/curled.jpg" alt="サムネイル"></a></p>
+								<p><a href="#">ブログタイトル</a></p>
+							</div>
+							<div class="cont">
+								<p><a href="#"><img src="image/odd.jpg" alt="サムネイル"></a></p>
+								<p><a href="#">ブログタイトル</a></p>
+							</div>
+							<div class="cont">
+								<p><a href="#"><img src="image/curled.jpg" alt="サムネイル"></a></p>
+								<p><a href="#">ブログタイトル</a></p>
+							</div>
+							<div class="cont">
+								<p><a href="#"><img src="image/odd.jpg" alt="サムネイル"></a></p>
+								<p><a href="#">ブログタイトル</a></p>
+							</div>
+							<div class="cont">
+								<p><a href="#"><img src="image/curled.jpg" alt="サムネイル"></a></p>
+								<p><a href="#">ブログタイトル</a></p>
+							</div>
+						</div>
+						<div class="arc wrap">
+							<h3>アーカイブ</h3>
+							<ul>
+								<li><a href="">2019年3月(10)</a></li>
+								<li><a href="">2019年2月(9)</a></li>
+								<li><a href="">2019年1月(8)</a></li>
+								<li><a href="">2018年12月(7)</a></li>
+								<li><a href="">2018年11月(6)</a></li>
+								<li><a href="">2018年10月(5)</a></li>
+							</ul>
+						</div>
+					</aside>
+				</div>
+			</main>
+			<footer>
+				<div class="flex">
+					<div class="me wrap">
+						<h3 class="auth">著者について<i class="fas fa-paw"></i></h3>
+						<p class="auth">実績など記入。</p>
+						<ul class="auth">
+							<li><a href="#">コンサル依頼の詳細</a></li>
+							<li><a href="#">Web制作依頼の詳細</a></li>
+							<li><a href="#">ライティング依頼来の詳細</a></li>
+							<li><a href="#">お問合せ</a></li>
+						</ul>
+					</div>
+					<div class="port wrap">
+					<h3 class="auth">ポートフォリオ<i class="fas fa-paw"></i></h3>
+					<ul>
+						<li><a href="">サイト1</a></li>
+						<li><a href="">サイト2</a></li>
+						<li><a href="">サイト3</a></li>
+					</ul>
+					</div>
+					<div class="twi wrap">
+						<h3 class="auth">Twitter<i class="fas fa-paw"></i></h3>
+						<p class="auth"><a href="https://twitter.com/twitter">Tweets by @Twitter</a></p>
+						<p>
+							<a href="https://twitter.com/intent/tweet?button_hashtag=ねこ部&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #ねこ部</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+						</p>
+						<button class="fixed_btn">
+						<i class="fas fa-paw"></i>
+					 	</button>
+					</div>
+				</div>
+				<p class="foot">
+					<small><a href="index.html">&copy;ねこブログ2019</a></small>
+				</p>
+			</footer>
+		</div>
+		<script>
+		$(function(){
+  			$(".fixed_btn,.fixed_btn:after").on("click", function(){
+   			 $("html").animate({scrollTop: 0 }, 1000, "swing");
+ 			 });
+		});
+
+		$(window).resize(function(){
+			var w = $(window).width();
+			var x = 480;
+			if (w >= x) {
+				$(".title").removeClass("open");
+				$("nav").css({
+					display: 'block'
+				});
+			} else {
+				$('nav').css({
+					display: 'none'
+				});
+			};
+		});
+
+		$(function(){
+			$('#nToggle').click(function(){
+				$(".title").toggleClass('open');
+				if($(".title").hasClass('open')) {
+					$("nav").show(500);
+				} else {
+					$("nav").hide(500);
+				}
+			});
+		});
+
+		</script>
+	</body>
+</html>
+
